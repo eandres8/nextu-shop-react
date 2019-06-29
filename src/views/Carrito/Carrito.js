aa6b018a40585ea8d810f66a64063327911a89e2
Andres Vega <elb.andres8@gmail.com>
2019-06-28 23:50:33 -0500

    agrego vista de carrito

--- src/views/Carrito/Carrito.js
+++ src/views/Carrito/Carrito.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
 import AppFrame from '../../components/AppFrame/AppFrame';
+import { connect } from 'react-redux';
+
+import './Carrito.css';
+
+// actions
+import { setearProductos } from '../../actions/productos.action';
 
 class Carrito extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            carrito: this.props.carrito,
+        };
+        console.log(this.state);
+    }
+
+    sumaProductos() {
+        return this.state.carrito.reduce( ( valorAnterior, c ) => {
+            return valorAnterior + (c.cantidad * c.price);
+        }, 0 );
+    }
+
     render() {
         return (
             <div>
                 <AppFrame
                     body={
-                        <h1>Hola Carrito</h1>
+                        <div className="content-body-home">
+                            <nav>
+                                <div className="nav-wrapper">
+                                    <span className="left brand-logo color-fuente">Carrito de compras</span>
+                                </div>
+                            </nav>
+                            <div className="row">
+                                <div className="col s12 m6">
+                                    <ul className="collection">
+
+                                        {this.state.carrito.map( c => (
+                                            <li key={c.id} className="collection-item avatar">
+                                                <img src={c.img} alt="" className="circle" />
+                                                <span className="title">{c.name}</span>
+                                                <p>Unidades: {c.cantidad} <br />
+                                                    <strong>Subtotal:</strong> {c.cantidad * c.price}
+                                                </p>
+                                                <a href="#!" className="secondary-content">
+                                                    <i className="material-icons">grade</i>
+                                                </a>
+                                            </li>
+                                        ) )}
+
+                                    </ul>
+                                </div>
+
+                                <div className="col s12 m6">
+                                    <h3>Total: {this.sumaProductos()}</h3>
+                                    <br/>
+                                    <button className="waves-effect waves-light btn">
+                                        Cancelar
+                                    </button>
+                                    <button className="waves-effect waves-light btn">
+                                        Pagar
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     }>
                 </AppFrame>
             </div>
@@ -15,4 +73,12 @@ class Carrito extends Component {
     }
 }
 
-export default Carrito;
+const mapStateToProps = state => ({
+    carrito: state.productos.carrito
+});
+
+const mapDispatchToProps = dispatch => ({
+    _setearProductos: value => dispatch(setearProductos(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Carrito);
